d4bf2cb5a91c0836ab9f786fb77030739f358b0d
punk1d <punk1d@hotmail.com>
2019-05-10 02:14:34 -0500

    Created api client
    Api client created for "Client" model.

--- Experiment2/ApiClientsController.cs
+++ Experiment2/ApiClientsController.cs
@@ -14,33 +14,20 @@ namespace Experiment2
 
         public void CreateClient(ClientModel client)
         {
-            IRestResponse Response = null;
-            var RestClient = new RestClient("http://www.experiment2.io/api/clients");
-            var Request = new RestRequest(Method.POST);
-            Request.AddHeader("Content-Type", "application/json");
-            var Client = new Parameter("Client", client, ParameterType.RequestBody);
-            Request.AddParameter(Client);
-            //var Password = new Parameter("Password", client.Password, ParameterType.RequestBody);
-            //Request.AddParameter(Password);
-            //var name = new Parameter("name", client.Name, ParameterType.RequestBody);
-            //Request.AddParameter(name);
-            //var Lastname = new Parameter("Lastname", client.Lastname, ParameterType.RequestBody);
-            //Request.AddParameter(Lastname);
-            //var Phone = new Parameter("Phone", client.Phonenumber, ParameterType.RequestBody);
-            //Request.AddParameter(Phone);
-            //var Email = new Parameter("Email", client.Email, ParameterType.RequestBody);
-            //Request.AddParameter(Email);
-            try
-            {
-                Response = RestClient.Execute(Request);
-                
-                Console.WriteLine(Response.Content);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            Console.WriteLine(Response.Content);
+            var RestClient = new RestClient("http://www.experiment2.io");
+            //HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("api/clients", Method.POST);
+
+            // easily add HTTP Headers
+            request.AddHeader("Content-Type", "application/json");
+
+            request.AddJsonBody(client);
+
+            // execute the request
+            IRestResponse response = RestClient.Execute(request);
+            var content = response.Content;
+            Console.WriteLine(content);
         }
 
     }
